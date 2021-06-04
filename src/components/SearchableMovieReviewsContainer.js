a95c4c2df318d99f904ff915db6d9138eac42322
kurwitz3 <kurwitz3@aol.com>
2021-06-04 18:23:12 -0400

    commit tree bd5b11d96569e38efa243bbf356790fe3d3ca662

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,35 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor(){
+        super()
+        this.state={
+            reviews:[],
+            searchTerm:''
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(json => this.setState({reviews: json.results}))
+    }
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Movie:</label>
+                    <input></input>
+                    <button value='submit'>Submit</button>
+                    <MovieReviews />
+
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
